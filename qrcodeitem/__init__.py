2a0359409f1cec99f5ff47241a68f7bb0c2f1521
John G <gkahiu@gmail.com>
2020-07-20 21:20:37 +0300

    Add workflow template

---
+++ qrcodeitem/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+Name                 : QRBarCodePlugin
+Description          : Plugin for adding Qr Code and barcode items in a print
+                       layout
+Date                 : 07-07-2020
+copyright            : (C) 2020 by John Gitau
+                       See the accompanying file CONTRIBUTORS.txt in the root
+email                : gkahiu@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+from .plugin import QRBarCodePluginLoader
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load the plugin loader class
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    return QRBarCodePluginLoader(iface)
---
+++ qrcodeitem/plugin.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+Name                 : QRBarCodePluginLoader
+Description          : Loader for the QR and bar code layout items
+Date                 : 07-07-2020
+copyright            : (C) 2020 by John Gitau
+                       See the accompanying file CONTRIBUTORS.txt in the root
+email                : gkahiu@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+import os.path
+
+from qgis.PyQt.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
+
+
+class QRBarCodePluginLoader:
+    """QGIS plugin loader."""
+
+    def __init__(self, iface):
+        """Constructor.
+
+        :param iface: An interface instance that will be passed to this class
+            which provides the hook by which you can manipulate the QGIS
+            application at run time.
+        :type iface: QgsInterface
+        """
+        self.iface = iface
+        # initialize plugin directory
+        self.plugin_dir = os.path.dirname(__file__)
+        # initialize locale
+        locale = QSettings().value('locale/userLocale')[0:2]
+        locale_path = os.path.join(
+            self.plugin_dir,
+            'i18n',
+            'qrc_{}.qm'.format(locale))
+
+        if os.path.exists(locale_path):
+            self.translator = QTranslator()
+            self.translator.load(locale_path)
+
+            if qVersion() > '4.3.3':
+                QCoreApplication.installTranslator(self.translator)
+
+    # noinspection PyMethodMayBeStatic
+    def tr(self, message): # pylint: disable=no-self-use
+        """Get the translation for a string using Qt translation API.
+
+        We implement this ourselves since we do not inherit QObject.
+
+        :param message: String for translation.
+        :type message: str, QString
+
+        :returns: Translated version of message.
+        :rtype: QString
+        """
+        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
+        return QCoreApplication.translate('QrBarCodeLayoutItem', message)
+
+    def initGui(self):
+        """Register items in the QGIS layout registry."""
+        self.register_layout_items()
+
+    def register_layout_items(self):
+        """Register QR and bar code items."""
+        pass
+
+    def unregister_layout_items(self):
+        """Unregister QR and bar code items."""
+        pass
+
+    def unload(self):
+        """Removes the qr and bar code items from the QGIS layout registry."""
+        self.unregister_layout_items()
